bde9827089ee074880480a341b02589090272ddb
Dominic Hofilena <dnickz25@gmail.com>
2012-08-14 11:23:22 +0800

    test

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Lean Body System</title>
+</head>
+
+<body bgcolor="#CCCCCC" style="margin:0px; padding:0px;">
+<center>
+<a href="http://addf0jlitym9uh0cifupwfu3u2.hop.clickbank.net/">
+ <img src="weightloss.jpg" border="0px" />
+</a>
+</center>
+</body>
+</html>
